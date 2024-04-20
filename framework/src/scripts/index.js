1402a24193daaffc9bafad19f191f3fdef0dbd36
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-20 13:27:31 -0700

    ♻️ Refactor HTML structure and update styles
    This commit introduces a significant refactor of the HTML structure, particularly within the framework's index.html file.
    
    Key modifications include introducing more descriptive class names for better readability, and optimizing the use of Bootstrap utilities for layout and components.
    
    The commit also addresses various styling issues across the framework, such as inconsistent padding, margin adjustments, and the removal of unnecessary styles. These changes contribute to a cleaner, more maintainable codebase.

--- framework/src/scripts/index.js
+++ framework/src/scripts/index.js
@@ -1,4 +1,15 @@
-import '../styles/bootstrap.scss';
-import '../styles/index.scss';
+import "../styles/bootstrap.scss";
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+// Import all of Bootstrap's JS
+import { Popover, Dropdown } from "bootstrap";
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+	new Popover(popover);
+});
+
+// Create an example dropdown
+document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach((dropdownToggleEl) => {
+	new Dropdown(dropdownToggleEl);
+});
