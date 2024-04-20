2af99126166bdb96b956db69924da0c107cc5878
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-20 16:58:51 -0700

    create 86 files, update 5 files and delete 1 file

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,1 +1,14 @@
 console.log(">> main");
+
+// Import all of Bootstrap's JS
+import { Popover, Dropdown } from "bootstrap";
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+	new Popover(popover);
+});
+
+// Create an example dropdown
+document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach((dropdownToggleEl) => {
+	new Dropdown(dropdownToggleEl);
+});
