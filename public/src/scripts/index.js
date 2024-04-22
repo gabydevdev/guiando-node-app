994c9d98bfe62c77516c91a6caa29bc547a3fcf4
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-22 11:58:39 -0700

    delete 1 file and update 1 file

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,5 +1,4 @@
 import { Popover, Dropdown } from 'bootstrap';
-import { fetchDataFromAPI } from './apiManager';
 
 document.addEventListener('DOMContentLoaded', async function () {
 	initializeBootstrapComponents();
@@ -50,8 +49,55 @@ async function createTableWithData(selector) {
 	});
 }
 
+export async function fetchDataFromAPI(params) {
+	const query = new URLSearchParams(params).toString();
+	const apiUrl = `http://localhost:3000/api/test?${query}`;
+
+	console.log(apiUrl);
+
+	try {
+		const response = await fetch(apiUrl);
+		const result = await response.json();
+		return result.data.map((item) => {
+			const activityBookings = cleanData(item);
+			return {
+				"status": item.status,
+				"bookingId": item.bookingId,
+				"creationDate": item.creationDate,
+				"externalId": activityBookings.activity.externalId,
+				"dateString": activityBookings.dateString,
+				"totalParticipants": activityBookings.totalParticipants,
+				"totalPrice": item.totalPrice,
+			};
+		});
+	} catch (error) {
+		console.error('There was a problem with your fetch operation:', error);
+	}
+}
+
 function addCell(row, text) {
 	const cell = document.createElement('td');
 	cell.textContent = text;
 	row.appendChild(cell);
+}
+
+function cleanData(item) {
+	const cleanedActivityBookings = JSON.parse(
+		item.activityBookings
+			.replace(/'/g, '"')
+			.replace(/False/g, 'false')
+			.replace(/True/g, 'true')
+			.replace(/None/g, 'null')
+			.replace(
+				/(?<=[A-Za-z0-9])"(?=[A-Za-z0-9])/g,
+				'SINGLE_QUOTE_STANDBY'
+			)
+			.replace(/\\x/g, '')
+			.replace(/="/g, '=&quot;')
+			.replace(/;"/g, ';&quot;')
+			.replace(/">/g, '&quot;&gt;')
+			.replace(/SINGLE_QUOTE_STANDBY/g, "'")
+	);
+
+	return cleanedActivityBookings[0];
 }
