14a4fb9f6d25f5a7e7da6fbc20cddde2abfd1a9c
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-26 13:57:14 -0700

    * refactor(dataTables.js): change single quotes to double quotes for consistency * feat(dataTables.js): add ordering by 'travelDate' column in ascending order * feat(dataTables.js): adjust length menu options to [6, 12, 24, 50]

--- public/src/scripts/dataTables.js
+++ public/src/scripts/dataTables.js
@@ -1,70 +1,106 @@
 // dataTables.js
-import DataTable from 'datatables.net-dt';
+import DataTable from "datatables.net-dt";
 
 export function setupDataTable(selector, apiURL) {
-	return new DataTable(selector, {
+	const table = new DataTable(selector, {
 		processing: true,
 		serverSide: true,
 		ajax: {
 			url: apiURL,
 		},
 		columns: [
-			{ data: 'bookingId', title: 'Booking Id' },
+			{ data: "bookingId", title: "Booking Id", name: "bookingId" },
 			{
-				data: 'creationDate',
+				data: "creationDate",
 				render: function (data, type) {
-					var d = new Date(data);
+					let d = new Date(data);
 
-					if (type === 'display') {
-						return d.toLocaleDateString('en-US', {
-							month: '2-digit',
-							year: 'numeric',
-							day: '2-digit',
+					if (type === "display") {
+						return d.toLocaleDateString("en-US", {
+							month: "2-digit",
+							year: "numeric",
+							day: "2-digit",
 						});
 					}
 					return data;
 				},
-				title: 'Created On',
-				className: 'created-on date',
+				title: "Created On",
+				name: "creationDate",
+				className: "created-on date",
 			},
 			{
-				data: 'activityBookings[0].product.externalId',
-				title: 'External ID',
+				data: "activityBookings[0].product.externalId",
+				title: "External ID",
+				name: "externalId",
 			},
 			{
-				data: 'customer',
+				data: "customer",
 				render: function (data) {
 					return `${data.firstName} ${data.lastName}`;
 				},
-				title: 'Traveler Name',
+				title: "Traveler Name",
+				name: "customer",
 			},
-			{ data: 'customer.phoneNumber', title: 'Phone #' },
+			{ data: "customer.phoneNumber", title: "Phone #", name: "phoneNumber" },
 			{
-				data: 'activityBookings[0].dateString',
-				title: 'Travel Date',
-				className: 'travel-date date',
+				data: "activityBookings[0].dateString",
+				render: function (data, type) {
+					let d = new Date(data);
+
+					if (type === "display") {
+						const date = d.toLocaleDateString(undefined, {
+							weekday: "long",
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						});
+						const time = d.toLocaleTimeString("en-US", {
+							hour: "2-digit",
+							minute: "2-digit",
+							hour12: true,
+						});
+
+						return `${date}, ${time}`;
+					}
+					return data;
+				},
+				title: "Travel Date",
+				name: "travelDate",
+				className: "travel-date date",
 			},
-			{ data: 'activityBookings[0].totalParticipants', title: '# PAX' },
+			{ data: "activityBookings[0].totalParticipants", title: "# PAX", name: "pax" },
 			{
-				data: 'totalPrice',
-				title: 'Total Price',
-				className: 'price currency',
+				data: "totalPrice",
+				title: "Total Price",
+				name: "totalPrice",
+				className: "price currency",
 			},
-			{ data: 'status', title: 'Status', className: 'status' },
+			{ data: "status", title: "Status", name: "status", className: "status" },
 		],
 		paging: true,
 		autoWidth: false,
 		pageLength: 12,
-		lengthMenu: [5, 10, 25, 50],
+		lengthMenu: [6, 12, 24, 50],
+		ordering: true,
+		order: {
+			name: "travelDate",
+			dir: "asc"
+		},
 		retrieve: true,
 		initComplete: function () {
-			console.log('DataTables has finished its initialisation.');
+			console.log("DataTables has finished initialisation.");
 
-			const evt = new Event("tableComplete", { bubbles: true, cancelable: false });
+			const evt = new Event("tableComplete", {
+				bubbles: true,
+				cancelable: false,
+			});
 			document.dispatchEvent(evt);
 		},
 	});
 
 	// @see https://datatables.net/reference/api/init()#Description
 	// console.log(table.init());
+
+	var order = table.order();
+	console.log("Column " + order + " is the ordering column");
 }
