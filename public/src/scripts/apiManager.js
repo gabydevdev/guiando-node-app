0fcc22e3524479d051a1d67e2a79ecdb458e2671
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-30 23:04:22 -0700

    * refactor(apiManager.js): optimize fetchDataFromAPI function

--- public/src/scripts/apiManager.js
+++ public/src/scripts/apiManager.js
@@ -1,60 +1,15 @@
 // apiManager.js
+
+// Function to fetch bookings data from API
 export async function fetchDataFromAPI(apiURL, params) {
 	const query = new URLSearchParams(params).toString();
 
-	apiURL = `${apiURL}?${query}`;
-	console.log("apiURL: ", apiURL);
+	const url = `${apiURL}?${query}`;
+	console.log("url: ", url);
 
-	const fetchedData = await fetch(apiURL)
-		.then((response) => response.json())
-		.then((result) => {
-			let dataSet = result.data.map((item) => {
-				const bookingId = item.bookingId;
-				const status = item.status;
-				const creationDate = item.creationDate;
-				const totalPrice = item.totalPrice;
-
-				const dataArray = {
-					status: status,
-					bookingId: bookingId,
-					creationDate: creationDate,
-					totalPrice: totalPrice,
-				};
-				// console.log('dataArray: ', dataArray);
-				return dataArray;
-			});
-
-			// console.log('dataSet: ', dataSet);
-			return dataSet;
-		})
-		.catch((error) => {
-			console.error(
-				"There was a problem with your fetch operation:",
-				error
-			);
-		});
-
-	return fetchedData;
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error("Failed to fetch bookings data");
+	}
+	return await response.json();
 }
-
-// Cleaning function for JSON
-// function cleanData(string) {
-// 	const formattedData = JSON.parse(
-// 		string
-// 			.replace(/'/g, '"')
-// 			.replace(/False/g, "false")
-// 			.replace(/True/g, "true")
-// 			.replace(/None/g, "null")
-// 			.replace(
-// 				/(?<=[A-Za-z0-9])"(?=[A-Za-z0-9])/g,
-// 				"SINGLE_QUOTE_STANDBY"
-// 			)
-// 			.replace(/\\x/g, "")
-// 			.replace(/="/g, "=&quot;")
-// 			.replace(/;"/g, ";&quot;")
-// 			.replace(/">/g, "&quot;&gt;")
-// 			.replace(/SINGLE_QUOTE_STANDBY/g, "'")
-// 	);
-
-// 	return formattedData;
-// }
