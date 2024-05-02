dbb9b65053b67f1359829a0d21351b444a2432a0
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-05-01 17:20:23 -0700

    📦 NEW: * refactor(request.js): code for populating table with booking data using localStorage

--- public/src/pages/request/request.js
+++ public/src/pages/request/request.js
@@ -1,80 +1,25 @@
-// // request.js
-// import { fetchDataFromAPI } from '@scripts/apiManager.js';
+// request.js
+document.addEventListener("DOMContentLoaded", async function () {
+	const selectedBookings =
+		JSON.parse(localStorage.getItem("selectedBookings")) || [];
 
-// let apiURL = process.env.API_URL + '/api/bookings';
+	const bookingTable = document.querySelector("#matrix_1 tbody");
 
-// let params = {
-// 	start: 0,
-// 	length: 20,
-// };
+	// Populate table with bookings data
+	selectedBookings.forEach((booking, i) => {
+		const bookingRow = bookingTable.rows[i];
 
-// document.addEventListener('DOMContentLoaded', async function () {
-// 	// Assuming `selectedBookings` is an array of booking IDs stored in localStorage or passed in some other way
-// 	const bookings = JSON.parse(localStorage.getItem('selectedBookings'));
+		const externalId = booking.activityBookings.product.externalId;
+		const customerName = `${booking.customer.firstName} ${booking.customer.lastName}`;
+		const pax = booking.activityBookings.totalParticipants;
+		const phoneNumber = booking.customer.phoneNumber;
 
-// 	if (!bookings || bookings.length === 0) {
-// 		let fetchData = await fetchDataFromAPI(apiURL, params);
-// 		fetchData.map(item => item.bookingId);
-// 		updateBookingsDisplay(fetchData);
-// 		// populateTourGuides();
-// 	} else {
-// 		// Display selected bookings
-// 		updateBookingsDisplay(bookings);
-// 		// populateTourGuides();
-// 	}
-// });
-
-// function updateBookingsDisplay(bookings) {
-// 	const select = document.getElementById('selectedBookings');
-// 	bookings.forEach(item => {
-// 		const option = document.createElement('option');
-// 		option.textContent = item.bookingId;
-// 		option.value = item.bookingId;
-// 		select.appendChild(option);
-// 	});
-// 	document.getElementById('selectedBookings').value = bookings.join(', ');
-// }
-
-// function populateTourGuides() {
-// 	const headers = new Headers();
-// 	headers.append('Content-Type', 'application/json');
-
-// 	const dataReq = new Request('./guias.json', {
-// 		method: 'GET',
-// 		mode: 'no-cors',
-// 		headers: headers,
-// 		destination: 'json',
-// 		credentials: 'omit',
-// 	});
-
-// 	console.log('dataReq: ', dataReq);
-
-// 	fetch(dataReq)
-// 		.then((res) => {
-// 			console.log('res: ', res);
-
-// 			if (!res.ok) {
-// 				throw new Error (`HTTP error! Status: ${res.status}`);
-// 			}
-// 			return res.json();
-// 		})
-// 		.then(data => {
-// 			console.log('data: ', data);
-
-// 			const select = document.getElementById('tourGuide');
-// 			data.forEach(person => {
-// 				const option = document.createElement('option');
-// 				option.textContent = `${person.firstName} ${person.lastName}`;
-// 				option.value = person.userId;
-// 				select.appendChild(option);
-// 			});
-// 		})
-// 		.catch((error) => {
-// 			console.error(
-// 				'There was a problem with your fetch operation:',
-// 				error
-// 			);
-// 		});
-// }
-
-// populateTourGuides();
+		bookingRow.querySelector("[data-matrix-answer='Reference #']").value =
+			externalId;
+		bookingRow.querySelector("[data-matrix-answer='Traveler Name']").value =
+			customerName;
+		bookingRow.querySelector("[data-matrix-answer='# PAX']").value = pax;
+		bookingRow.querySelector("[data-matrix-answer='Phone']").value =
+			phoneNumber;
+	});
+});
