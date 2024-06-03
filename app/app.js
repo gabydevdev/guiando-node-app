fd3c9536da817b2d0c9536ed8dbd123050fd7546
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-06-03 13:44:02 -0700

    Revert "Add date range filter to /api/bookings endpoint"
    This reverts commit a69cbbd5a29ce1fa3959a0a91737be428f2499ae.

--- app/app.js
+++ app/app.js
@@ -20,12 +20,12 @@ app.use(express.json());
 
 const bookingsDataLogs = path.join(__dirname, "booking_data");
 
-const page = parseInt(req.query.page) || 1;
-const limit = parseInt(req.query.limit) || 12;
-const sortBy = req.query.sortBy || "creationDate"; // Default sort by creationDate
-const order = req.query.order || "desc"; // Default sort order
-const startDate = new Date(req.query.startDate);
-const endDate = new Date(req.query.endDate);
+app.get(`${baseUrlPath}/api/bookings`, (req, res) => {
+	const page = parseInt(req.query.page) || 1;
+	const limit = parseInt(req.query.limit) || 12;
+	const sortBy = req.query.sortBy || "creationDate"; // Default sort by creationDate
+	const order = req.query.order || "desc"; // Default sort order
+
 	fs.readdir(bookingsDataLogs, (err, files) => {
 		if (err) {
 			console.error("Could not list the directory.", err);
