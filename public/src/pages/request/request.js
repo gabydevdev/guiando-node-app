e1bf0262acc467f85829d7124259899617863739
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-06-06 19:08:49 -0700

    * refactor(pages/request): refactor form fields and layout for better readability and user experience * chore(request.js): update logic to fetch data from API and populate table with bookings data * style(request.scss): add import statement for form elements module * chore(index.js): add localeStringShort constant for date formatting

--- public/src/pages/request/request.js
+++ public/src/pages/request/request.js
@@ -1,12 +1,12 @@
 // request.js
 console.log(">> Request page");
 
-import { apiURL } from "@scripts/index.js";
+import { apiURL, localeStringShort } from "@scripts/index.js";
 import { fetchDataFromAPI } from "@scripts/apiManager.js";
 
 let bookings, startDate, endDate;
 let currentPage = 1;
-let limit = 10;
+let limit = -1;
 let params = {
 	limit: limit,
 	page: currentPage,
@@ -14,70 +14,130 @@ let params = {
 };
 
 document.addEventListener("DOMContentLoaded", async function () {
-	const startDateField = document.querySelector("#date_1");
-	const endDateField = document.querySelector("#date_2");
+	bookings = await fetchDataFromAPI(apiURL, params);
+	console.log("all bookings: ", bookings.total);
 
-	// Enable end date field if start date has a value
-	startDateField.addEventListener("change", function () {
-		if (startDateField.value) {
-			endDateField.disabled = false;
-		} else {
-			endDateField.disabled = true;
-			endDateField.value = ""; // Clear end date if start date is cleared
-		}
+	// End Date Field
+	const endDateField = document.getElementById("date_2");
 
-		console.log(startDateField.value);
-	});
+	// If start date field is empty, end date field should be disabled.
+	document
+		.getElementById("date_1")
+		.addEventListener("change", (event) => {
+			startDate = event.target.value;
 
-	// Run the API function on end date value change
-	endDateField.addEventListener("change", async function () {
-		startDate = startDateField.value;
-		endDate = endDateField.value;
+			if (startDate) {
+				endDateField.disabled = false;
+			} else {
+				// Clear and disable end date field if start date is cleared.
+				endDateField.disabled = true;
+				endDateField.value = "";
+			}
 
-		if (!startDate || !endDate) {
-			return;
-		}
+			console.log('startDate: ', startDate);
+			return startDate;
+		});
 
-		// Define API parameters
-		params = {
-			limit: limit,
-			page: currentPage,
-			sortBy: "startDateTime",
-			startDate: startDate,
-			endDate: endDate,
-		};
+	console.log('startDate: ', startDate);
 
-		// Fetch data from API using the date fields as parameters
-		try {
-			bookings = await fetchDataFromAPI(apiURL, params);
-			// console.log("Bookings: ", bookings);
-			console.log("Total Bookings: ", bookings.total);
-			// Update total entries caption
-			document.getElementById("totalEntries").textContent = bookings.total;
+	endDateField
+		.addEventListener("change", (event) => {
+			endDate = event.target.value;
+			console.log('endDate: ', endDate);
 
-			// Update the table with the fetched bookings data
-			populateTable(bookings.data);
-		} catch (error) {
-			console.error("Error fetching data from API: ", error);
-			alert("Failed to fetch data from API. Please try again.");
-		}
-	});
+			if (!startDate || !endDate) {
+				return;
+			}
+
+			// Define API parameters
+			params = {
+				limit: limit,
+				page: currentPage,
+				sortBy: "startDateTime",
+				startDate: startDate,
+				endDate: endDate,
+			};
+
+			populateTable(apiURL, params);
+		});
 });
 
 // Function to populate the table with bookings data
-function populateTable(bookingsData) {
-	const bookingTable = document.querySelector("#matrix_1 tbody");
+async function populateTable(apiURL, params) {
+	const bookings = await fetchDataFromAPI(apiURL, { ...params });
+	const bookingsData = bookings.data;
+
+	console.log('total bookings: ', bookings.total);
+
+	// Update total entries caption
+	document.getElementById("totalEntries").textContent = bookings.total;
+
+	const tableBody = document.querySelector("#checkbox_2 tbody");
+	tableBody.innerHTML = "";
 
 	bookingsData.forEach((booking, i) => {
-		const bookingRow = bookingTable.rows[i];
+		// Booking values required
 		const { externalId } = booking.activityBookings.product;
 		const customerName = `${booking.customer.firstName} ${booking.customer.lastName}`;
+
+		const invoiceDates = booking.activityBookings.invoice.dates;
+		const invoiceDatesFormatted = new Date(invoiceDates).toLocaleString(
+			"en-US",
+			localeStringShort
+		);
+
 		const pax = booking.activityBookings.totalParticipants;
 		const phoneNumber = booking.customer.phoneNumber;
 
-		bookingRow.querySelector("[data-matrix-answer='Reference #']").value = externalId;
-		bookingRow.querySelector("[data-matrix-answer='Traveler Name']").value = customerName;
-		bookingRow.querySelector("[data-matrix-answer='# PAX']").value = pax;
-		bookingRow.querySelector("[data-matrix-answer='Phone']").value = phoneNumber;
+		// Collect values into an object
+        const bookingValues = {
+            externalId: externalId,
+            customerName: customerName,
+            invoiceDates: invoiceDates,
+            pax: pax,
+            phoneNumber: phoneNumber
+        };
+
+        // Convert object to JSON string
+        const bookingValuesJSON = JSON.stringify(bookingValues);
+
+		// Create table row
+		const row = tableBody.insertRow();
+
+		// Create table cell with checkbox
+		const cell_cb = row.insertCell();
+		cell_cb.id = "cell_cb";
+
+		const cb = document.createElement("input");
+		cb.setAttribute('type', 'checkbox');
+		cb.setAttribute('name', 'checkbox_2[]');
+		cb.setAttribute('id', 'checkbox_2_' + i);
+
+		cb.value = bookingValuesJSON;
+
+		cb.setAttribute('data-refid', externalId);
+		cb.setAttribute('data-name', customerName);
+		cb.setAttribute('data-date', invoiceDates);
+		cb.setAttribute('data-pax', pax);
+		cb.setAttribute('data-phone', phoneNumber);
+
+		cell_cb.appendChild(cb);
+
+		// Create info table cells
+		//
+		// Reference #
+		row.insertCell().textContent = externalId;
+
+		// Traveler Name
+		row.insertCell().textContent = customerName;
+
+		// Tour Date
+		row.insertCell().textContent = invoiceDatesFormatted;
+
+		// # PAX
+		row.insertCell().textContent = pax;
+
+		// Phone
+		row.insertCell().textContent = phoneNumber;
 	});
 }
